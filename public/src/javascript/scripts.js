859ca8a8663747138cc640fd3882f992f49735e4
DARKPARASITEPC\rishi <rishikhannalive@gmail.com>
2024-09-18 11:00:48 +0530

    Deploy Test

---
+++ public/src/javascript/scripts.js
@@ -0,0 +1,16 @@
+
+// Scroll to Top
+function scrollToTop() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+window.addEventListener('scroll', function () {
+    const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+    if (document.documentElement.scrollTop > 120) {
+        scrollToTopBtn.style.pointerEvents = 'auto'; // Enable button interaction
+        scrollToTopBtn.style.opacity = '1'; // Fade in the button
+    } else {
+        scrollToTopBtn.style.pointerEvents = 'none'; // Disable button interaction while hidden
+        scrollToTopBtn.style.opacity = '0'; // Fade out the button
+    }
+});
