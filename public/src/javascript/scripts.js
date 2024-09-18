64c69130396d7152b1f545bce08b2614f06a3366
DARKPARASITEPC\rishi <rishikhannalive@gmail.com>
2024-09-18 16:09:57 +0530

    Mobile Compatibility Test

--- public/src/javascript/scripts.js
+++ public/src/javascript/scripts.js
@@ -1,10 +1,10 @@
-
 // Scroll to Top
 function scrollToTop() {
     window.scrollTo({top: 0, behavior: 'smooth'});
 }
 
-window.addEventListener('scroll', function () {
+window.addEventListener('scroll', function ()
+{
     const scrollToTopBtn = document.getElementById('scrollToTopBtn');
     if (document.documentElement.scrollTop > 120) {
         scrollToTopBtn.style.pointerEvents = 'auto'; // Enable button interaction
@@ -13,4 +13,10 @@ window.addEventListener('scroll', function () {
         scrollToTopBtn.style.pointerEvents = 'none'; // Disable button interaction while hidden
         scrollToTopBtn.style.opacity = '0'; // Fade out the button
     }
+});
+
+// JavaScript to toggle the mobile menu
+document.getElementById('menu-toggle').addEventListener('click', function () {
+    let menu = document.getElementById('mobile-menu');
+    menu.classList.toggle('open');
 });
